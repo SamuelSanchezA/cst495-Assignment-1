0b4e3bef48cd2dba76de8558b967ee136de7ba5c
= <=>
2018-02-01 22:42:51 -0800

    Added alert if no internet connection detected

--- CST495-Assignment-1/NowPlayingViewController.swift
+++ CST495-Assignment-1/NowPlayingViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 import AlamofireImage
 
-class NowPlayingViewController: UIViewController, UITableViewDataSource {
+class NowPlayingViewController: UIViewController, UITableViewDataSource{
 
     @IBOutlet var movieTableView: UITableView!
     var movies : [[String: Any]] = []
     var refreshControl : UIRefreshControl!
+    var filteredData: [String]!
     
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
@@ -35,6 +36,16 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     func fetchMovies(){
         
+        let alertController = UIAlertController(title: "Cannot Get Movies", message: "The Internet Connection appears to be offline", preferredStyle: .alert)
+        
+        // create a cancel action
+        let tryAgainAction = UIAlertAction(title: "Try Again", style: .default) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+            self.fetchMovies()
+        }
+        // add the cancel action to the alertController
+        alertController.addAction(tryAgainAction)
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -42,6 +53,9 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
             // This will run when the network request returns
             if let error = error {
                 print(error.localizedDescription)
+            UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+                }
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
