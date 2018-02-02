7eaaec24af7977017d3af93a84dfb7b64875d32d
= <=>
2018-02-01 19:05:48 -0800

    Initial Commit

---
+++ CST495-Assignment-1Tests/CST495_Assignment_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CST495_Assignment_1Tests.swift
+//  CST495-Assignment-1Tests
+//
+//  Created by Samuel on 2/1/18.
+//  Copyright © 2018 Samuel. All rights reserved.
+//
+
+import XCTest
+@testable import CST495_Assignment_1
+
+class CST495_Assignment_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
